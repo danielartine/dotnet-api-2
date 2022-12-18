66890bc0c7ee428bdbea204dcf5d0839b74606b8
Daniel Artine Mastub <daniel.mastub@stoneage.com.br>
2022-12-18 16:35:29 -0300

    up

---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data;
+
+public class FilmeContext : DbContext
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts)
+        : base(opts)
+    {
+
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+}
