efabf5aa44aa6351169a39ebb4d66c9d6a6e2157
Daniel Artine Mastub <daniel.mastub@stoneage.com.br>
2022-12-19 21:05:57 -0300

    Aula-2

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -15,5 +15,4 @@ public class Filme
     [Required]
     [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
-    public int SessaoId { get; set; }
 }
